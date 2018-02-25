f11fac190146e4780c5f7c394f9b531ff952e52d
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2018-02-25 18:39:18 +0800

    Imported existing project
    Imported the existing project in to GitHub.

---
+++ Provectus Plasma/Assets/Scripts/Editor/ToggleButtonReset.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ToggleButton))]
+public class ToggleButtonReset : Editor 
+{
+	#region Variables and References
+	#endregion
+	
+	#region Methods
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ToggleButton toggleButton = (ToggleButton)target;
+
+        GUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Initialise"))
+        {
+            toggleButton.Initialise();
+        }
+
+        if (GUILayout.Button("Reset Colours"))
+        {
+            toggleButton.ResetValues(ResetType.Colours);
+        }
+
+        if (GUILayout.Button("Reset Size"))
+        {
+            toggleButton.ResetValues(ResetType.Size);
+        }
+
+        if (GUILayout.Button("Reset Delay"))
+        {
+            toggleButton.ResetValues(ResetType.Delay);
+        }
+        GUILayout.EndHorizontal();
+
+        toggleButton.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, toggleButton.size);
+        toggleButton.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, toggleButton.size);
+    }
+    #endregion
+}
---
+++ Provectus Plasma/Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    #region Variables and References
+    [Header("Colours")]
+    [SerializeField]
+    Color32 offColour = new Color32(127, 127, 255, 255);
+    [SerializeField]
+    Color onColour = new Color32(255, 192, 76, 255);
+
+    [Space]
+
+    [Header("Misc. Options")]
+    [SerializeField]
+    bool startOn;
+    [Range(1f, 1000f)]
+    public float size = 60f;
+    [Range(.01f, 5f)]
+    [SerializeField]
+    float delay = .5f;
+
+    bool currentState;
+    Button button;
+    Image buttonImage;
+    #endregion
+
+    #region Methods
+    // Use this for initialization
+    void Start()
+    {
+        buttonImage = gameObject.GetComponent<Image>();
+
+        Initialise();
+        currentState = startOn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Toggle()
+    {
+        currentState = !currentState;
+
+        if (currentState)
+        {
+            //TweenColours(onColour);
+            buttonImage.CrossFadeColor(onColour, delay, false, false);
+        }
+        else
+        {
+            //TweenColours(offColour);
+            buttonImage.CrossFadeColor(offColour, delay, false, false);
+        }
+    }
+
+    public void Initialise()
+    {
+        button = gameObject.GetComponent<Button>();
+        button.transition = Selectable.Transition.None;
+
+        Text buttonText = button.GetComponentInChildren<Text>();
+        buttonImage = gameObject.GetComponent<Image>();
+
+        if (buttonText != null)
+            Destroy(buttonText);
+
+        buttonImage.color = Color.white;
+
+        if (startOn)
+            buttonImage.CrossFadeColor(onColour, 0f, false, false);
+        else
+            buttonImage.CrossFadeColor(offColour, 0f, false, false);
+    }
+
+    public void ResetValues(ResetType resetType)
+    {
+        if (resetType == ResetType.Colours)
+        {
+            offColour = new Color32(127, 127, 255, 255);
+            onColour = new Color32(255, 192, 76, 255);
+        }
+        else if (resetType == ResetType.Size)
+        {
+            size = 60f;
+        }
+        else if (resetType == ResetType.Delay)
+        {
+            delay = .5f;
+        }
+    }
+    #endregion
+}
+
+public enum ResetType
+{
+    Colours, Size, Delay
+}
