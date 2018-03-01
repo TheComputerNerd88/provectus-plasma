ac32e6289a06bdcfabb638bf856c69c90b433934
TheComputerNerd88 <36807490+TheComputerNerd88@users.noreply.github.com>
2018-03-01 19:15:01 +0800

    Added AND Gate file

---
+++ Provectus Plasma/Assets/Scripts/AND Gate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANDGate : MonoBehaviour 
+{
+	#region Variables and References
+	#endregion
+	
+	#region Methods
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	#endregion
+}
